424d840062ed38fdf806b24f8e716af703b50a56
mrunalini1806 <74833605+mrunalini1806@users.noreply.github.com>
2020-11-26 02:58:21 +0530

    Add files via upload
    day2 practice completed

---
+++ variables practice-day2.py
@@ -0,0 +1,22 @@
+#exercise 1
+a=b=c = 23
+print (a/10)
+print (b*50)
+print (c+60)
+
+#exercise 2
+string = "check"
+ch = 'G'
+i =2
+result = string[:i]+ch+string[i+1:]
+print (result)
+
+#exercise 3
+
+a ,b = 10, 2.6
+a = float(a)
+print (type(a))
+print (a)
+b= int(b)
+print (type(b))
+print(b)
