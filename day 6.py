fa372261b8ca5220c754b3444ec83947f230afc3
Shaza majid <74833605+mrunalini1806@users.noreply.github.com>
2020-11-30 07:19:54 +0530

    Add files via upload

---
+++ day 6.py
@@ -0,0 +1,22 @@
+# exercise 1
+list1 = [1,2,3,4,5]
+list2 = []
+for i in list1:
+    i= i+2
+    list2.append(i)
+print(list2)
+
+#program to get pattern
+my_list = [5,4,3,2,1]
+patt = []
+for i in range(0,len(my_list)):
+    print(my_list)
+    i=i-1
+
+#multiplication
+list1 = [1,2,3,4,5,6,7,8,9]
+list2 = []
+for i in range(0,len(list1)):
+    i= i*9
+    list2.append(i)
+print(list2)
