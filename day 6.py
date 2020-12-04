b8352cea365caa80ed2f1ca29bbf5f2569aba71a
Shaza majid <74833605+mrunalini1806@users.noreply.github.com>
2020-12-05 02:21:59 +0530

    Add files via upload

--- day 6.py
+++ day 6.py
@@ -7,16 +7,26 @@ for i in list1:
 print(list2)
 
 #program to get pattern
-my_list = [5,4,3,2,1]
-patt = []
-for i in range(0,len(my_list)):
-    print(my_list)
-    i=i-1
+patt = [5,4,3,2,1]
+for i in range(5,0,-1):
+            print (patt)
+i-1
+print()
+
+
+
 
 #multiplication
-list1 = [1,2,3,4,5,6,7,8,9]
-list2 = []
-for i in range(0,len(list1)):
-    i= i*9
-    list2.append(i)
-print(list2)
+
+for i in range(1,11,1):
+    r= i*9
+    print ("9 * ", i , " = ",r)
+    i = i+1
+print()
+
+
+#fibonacci series
+x = 0
+for a in range(1,5,1):
+    x= x+a
+    print(a,x)
